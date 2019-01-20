51d2d6b1205a9bbe98defca5c95e345381b2acff
Tom Frakey <bloodmooninteractive@gmail.com>
2019-01-19 20:49:49 -0700

    Created initial game controller scripts

---
+++ Game Controllers/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void HighscoreMenu()
+    {
+        SceneManager.LoadScene("Highscore Scene");
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MusicButton()
+    {
+
+    }
+}
---
+++ Game Controllers/OptionsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
